a22697a4cf10f9a16510a5cf577e6ce13f01714e
Mitchell <mingchenothin9@gmail.com>
2023-04-04 12:02:45 +1000

    build: babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-const helloDiv = document.createElement("div");
-helloDiv.innerHTML = "Hello from Javascript!";
-document.body.append(helloDiv);
+import {createRoot} from "react-dom/client";
+import Hello from "./Hello";
+import React from "react";
+
+// const helloDiv = document.createElement("div");
+// helloDiv.innerHTML = "Hello from Javascript!";
+// document.body.append(helloDiv);
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Hello />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -6,5 +7,30 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "build"),
     },
-    mode: "development",
+    // mode: "development",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "build"),
+        },
+        port: 8888,
+    },
+    module: {
+        // exclude node_modules
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+        ],
+    },
+    // pass all js files through Babel
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
+    },
 };
