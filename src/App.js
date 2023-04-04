dd74c8f754fbffc5957dce6d33d5a8207b2eb9cc
Mitchell <mingchenothin9@gmail.com>
2023-04-04 16:39:32 +1000

    build: Plot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Plot from "./Plot";
 
 function App() {
     return (
         <div>
             <Header buttonType={"stop"} />
+            <Plot />
         </div>
     );
 }
