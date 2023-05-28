fdcd322755b3286b579b8b6eca97477fc568d580
Mitchell <mingchenothin9@gmail.com>
2023-05-28 18:02:13 +1000

    feat: README update

--- src/RawPlot.jsx
+++ src/RawPlot.jsx
@@ -80,7 +80,7 @@ const RawPlot = ({ isPlotting }) => {
     return (
         <div>
             {/*<p>Raw data process: {time} </p>*/}
-            <p>Real time: {realTime} </p>
+            {/*<p>Real time: {realTime} </p>*/}
             <BrainwaveGraph data={data} />
         </div>
     );
